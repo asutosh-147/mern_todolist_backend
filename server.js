33f03c8e4ec51f6448c11f33c2603067e8000561
asutosh-147 <98374996+asutosh-147@users.noreply.github.com>
2023-06-24 13:36:56 +0530

    cors added

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express=require('express');
 const { default: mongoose } = require('mongoose');
 const workoutRoutes=require('./routes/workouts');
 const userRoutes=require('./routes/user');
+const cors = require('cors');
+
 //create express app
 const app = express();
 const PORT = process.env.PORT || 4000;
 //middle ware
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use((req,res,next)=>{
